422e4e5e48cd97363175e3fee7862ec892e2f87b
rajansurya <rajansurya.88@gmail.com>
2026-01-10 15:38:34 +0530

    img

--- app/src/main/java/co/com/mypt/onBoarding/personalize/GenderFragment.kt
+++ app/src/main/java/co/com/mypt/onBoarding/personalize/GenderFragment.kt
@@ -29,27 +29,17 @@ class GenderFragment : Fragment() {
             R.drawable.image_gender_female_unselected,
             R.drawable.image_gender_other_unselected
         )
-//        listOf(
-//            R.drawable.male_,
-//            R.drawable.female_,
-//            R.drawable.genderother,
-//        )
+
         val selectedImageList = listOf(
             R.drawable.image_gender_male_selected,
             R.drawable.image_gender_female_selected,
             R.drawable.image_gender_other_selected
         )
-//        listOf(
-//            R.drawable.selectedmale,
-//            R.drawable.selectedfemale,
-//            R.drawable.selectedgenderother,
-//        )
+
         val iconList = listOf(
             R.drawable.im_male,
             R.drawable.im_female,
-            R.drawable.im_group,
-
-            )
+            R.drawable.im_group,)
         val textList =
             listOf(getString(R.string.male), getString(R.string.female), getString(R.string.others))
         val genderAdapter =
