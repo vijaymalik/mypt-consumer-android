68f4274780dce7b6afc9f4bbe048bd175452b4ce
rajansurya <rajansurya.88@gmail.com>
2026-02-10 12:28:22 +0530

    gridview

--- app/src/main/java/co/com/mypt/adapter/TrainerGridViewAdapter.kt
+++ app/src/main/java/co/com/mypt/adapter/TrainerGridViewAdapter.kt
@@ -13,8 +13,6 @@ import android.widget.TextView
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import co.com.mypt.R
-import co.com.mypt.activities.AddressListForTrainerActivity
-import co.com.mypt.activities.BookSlot
 import co.com.mypt.activities.TrainerDetails
 import co.com.mypt.model.ExerciseModel
 import co.com.mypt.model.TrainersModel
@@ -28,32 +26,36 @@ class TrainerGridViewAdapter(
     var latitude: Double?,
     var longitude: Double?,
     var studio_id: String,
-    var clickListener:(Boolean,String, String)->Unit
+    var clickListener: (Boolean, String, String) -> Unit
 ) : RecyclerView.Adapter<TrainerGridViewAdapter.ViewHolder>() {
-    lateinit var sharedPreferences:SharedPreferences
+    lateinit var sharedPreferences: SharedPreferences
     private var trainerListModels: List<TrainersModel>
 
-    class ViewHolder(item:View) : RecyclerView.ViewHolder(item) {
-        val exerciseRecyclerView : RecyclerView = item.findViewById(R.id.exerciseRecyclerView)
-        val bookSlot : TextView = item.findViewById(R.id.bookSlot)
-//        val im_verified : ImageView = item.findViewById(R.id.im_verified)
-        val avgRating : TextView = item.findViewById(R.id.avgRating)
-        val totalRatings : TextView = item.findViewById(R.id.totalRatings)
-        val userName : TextView = item.findViewById(R.id.userName)
-        val distance : TextView = item.findViewById(R.id.distance)
-        val place : TextView = item.findViewById(R.id.place)
-//        val viewProfile : TextView = item.findViewById(R.id.viewProfile)
+    class ViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val exerciseRecyclerView: RecyclerView = item.findViewById(R.id.exerciseRecyclerView)
+        val bookSlot: TextView = item.findViewById(R.id.bookSlot)
+
+        //        val im_verified : ImageView = item.findViewById(R.id.im_verified)
+        val avgRating: TextView = item.findViewById(R.id.avgRating)
+        val totalRatings: TextView = item.findViewById(R.id.totalRatings)
+        val userName: TextView = item.findViewById(R.id.userName)
+        val distance: TextView = item.findViewById(R.id.distance)
+        val place: TextView = item.findViewById(R.id.place)
+
+        //        val viewProfile : TextView = item.findViewById(R.id.viewProfile)
         //val hurryUp : TextView = item.findViewById(R.id.hurryUp)
 //        val availableSlots : TextView = item.findViewById(R.id.availableSlots)
-        val trainerImage : ImageView = item.findViewById(R.id.trainerImage)
-        val relative : RelativeLayout = item.findViewById(R.id.relative)
+        val trainerImage: ImageView = item.findViewById(R.id.trainerImage)
+        val relative: RelativeLayout = item.findViewById(R.id.relative)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = if(type == "Linear")
-            LayoutInflater.from(parent.context).inflate(R.layout.trainer_list_linear_adapter_layout, parent, false)
+        val view = if (type == "Linear")
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.trainer_list_linear_adapter_layout, parent, false)
         else
-            LayoutInflater.from(parent.context).inflate(R.layout.trainer_list_grid_adapter_layout, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.trainer_list_home_grid_adapter_layout, parent, false)
 
         return ViewHolder(view)
     }
@@ -63,96 +65,67 @@ class TrainerGridViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var trainersModel=trainerListModels[position]
-        sharedPreferences=PreferenceManager.getDefaultSharedPreferences(context)
-        val exerciseList = ArrayList<ExerciseModel>()
-        /*if (type=="grid"){
-            val layoutParams = holder.itemView.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.rightMargin = 15 // Set left margin in pixels
-            holder.itemView.layoutParams = layoutParams
-        }*/
-       /* if (trainersModel.is_verified.equals("true")){
-            holder.im_verified.visibility = View.VISIBLE
-        }else{
-            holder.im_verified.visibility = View.GONE
+        val trainersModel = trainerListModels[position]
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
-        }*/
-       /* for(i in 0 until trainersModel.activity.length()){
-            var jsonObject=trainersModel.activity.optJSONObject(i)
-            var exerciseModel=ExerciseModel()
-            exerciseModel.id=jsonObject.optString("id")
-            exerciseModel.name=jsonObject.optString("name")
-            exerciseList.add(exerciseModel)
-        }*/
-
-     val listTag=   trainersModel.tags.map {
-            var exerciseModel=ExerciseModel()
-            exerciseModel.id=it?.id.toString()
-            exerciseModel.name=it?.name.toString()
-         exerciseModel
+        val listTag = trainersModel.tags.map {
+            val exerciseModel = ExerciseModel()
+            exerciseModel.id = it?.id.toString()
+            exerciseModel.name = it?.name.toString()
+            exerciseModel
         }
-        holder.exerciseRecyclerView.adapter = TrainerTagAdapter(context,listTag,type)
+        holder.exerciseRecyclerView.adapter = TrainerTagAdapter(context, listTag, type)
 
         holder.bookSlot.tag = position
         holder.relative.tag = position
         holder.relative.setOnClickListener {
             val pos = it.tag as Int
-            var trainersModel=trainerListModels[pos]
+            var trainersModel = trainerListModels[pos]
             val intent = Intent(context, TrainerDetails::class.java)
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.putExtra("trainer_id",trainersModel.id)
-            intent.putExtra("studio_id",studio_id)
-            intent.putExtra("haveSlot",trainersModel.slot)
-            intent.putExtra("type",typeWorkout)
-            intent.putExtra("long",longitude)
-            Log.e("longitiude",""+longitude)
-            intent.putExtra("lat",latitude)
-            Log.e("lati",""+latitude)
-            Log.e("studio_id",""+studio_id)
+            intent.putExtra("trainer_id", trainersModel.id)
+            intent.putExtra("studio_id", studio_id)
+            intent.putExtra("haveSlot", trainersModel.slot)
+            intent.putExtra("type", typeWorkout)
+            intent.putExtra("long", longitude)
+            Log.e("longitiude", "" + longitude)
+            intent.putExtra("lat", latitude)
+            Log.e("lati", "" + latitude)
+            Log.e("studio_id", "" + studio_id)
             context.startActivity(intent)
         }
-       /* holder.viewProfile.setOnClickListener {
-            var trainersModel=trainerListModels[position]
-            val intent = Intent(context, TrainerDetails::class.java)
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.putExtra("trainer_id",trainersModel.id)
-            intent.putExtra("studio_id",studio_id)
-            intent.putExtra("haveSlot",trainersModel.slot)
-            intent.putExtra("type",typeWorkout)
-            intent.putExtra("long",longitude)
-            Log.e("longitiude",""+longitude)
-            intent.putExtra("lat",latitude)
-            Log.e("lati",""+latitude)
-            Log.e("studio_id",""+studio_id)
-            context.startActivity(intent)
-        }*/
         holder.userName.text = trainersModel.name
 
-        if(trainersModel.averageRating == "null")
+        if (trainersModel.averageRating == "null")
             holder.avgRating.text = "0"
         else
             holder.avgRating.text = trainersModel.averageRating
-        holder.totalRatings.text = trainersModel.noOfRating+" ratings"
+        holder.totalRatings.text = trainersModel.noOfRating + " ratings"
         holder.distance.text = trainersModel.distance
         holder.place.text = trainersModel.location
-        if(trainersModel.slot == "no"){
+        if (trainersModel.slot == "no") {
 //            holder.hurryUp.text = "No slots available"
             holder.bookSlot.setOnClickListener {}
             holder.bookSlot.background
 //            holder.availableSlots.visibility = View.GONE
-            holder.bookSlot.setBackgroundColor(context.resources.getColor(R.color.buttongreycolor,null))
-            holder.bookSlot.setTextColor(context.resources.getColor(R.color.white,null))
-        }
-        else {
+            holder.bookSlot.setBackgroundColor(
+                context.resources.getColor(
+                    R.color.buttongreycolor,
+                    null
+                )
+            )
+            holder.bookSlot.setTextColor(context.resources.getColor(R.color.white, null))
+        } else {
 //            holder.availableSlots.text = "Only ${trainersModel.slot} slots available"
             holder.bookSlot.setOnClickListener {
                 val pos = it.tag as Int
-                var trainersModel=trainerListModels[pos]
+                var trainersModel = trainerListModels[pos]
 
 //                if (trainersModel.is_group==false){
-                    val query=if (typeWorkout =="home") "type=home&trainer_id=${trainersModel.id}"  else "type=gym&trainer_id=${trainersModel.id}&studio_id=${trainersModel.studio_id}"
+                val query =
+                    if (typeWorkout == "home") "type=home&trainer_id=${trainersModel.id}" else "type=gym&trainer_id=${trainersModel.id}&studio_id=${trainersModel.studio_id}"
 
-                    clickListener(/*trainersModel.is_group*/true?:false,query,trainersModel.id)
+                clickListener(/*trainersModel.is_group*/true ?: false, query, trainersModel.id)
 //                    return@setOnClickListener
 //                }
                 /*if (sharedPreferences.getString("typeWorkout","").equals("home")){
@@ -181,10 +154,15 @@ class TrainerGridViewAdapter(
                 }*/
 
             }
-            holder.bookSlot.setBackgroundColor(context.resources.getColor(R.color.headingcolor,null))
+            holder.bookSlot.setBackgroundColor(
+                context.resources.getColor(
+                    R.color.headingcolor,
+                    null
+                )
+            )
 //            holder.hurryUp.text = context.resources.getString(R.string.hurry_up)
 //            holder.availableSlots.visibility = View.VISIBLE
-            holder.bookSlot.setTextColor(context.resources.getColor(R.color.buttontextcolor,null))
+            holder.bookSlot.setTextColor(context.resources.getColor(R.color.buttontextcolor, null))
         }
         Glide.with(context).load(trainersModel.profile).fitCenter().into(holder.trainerImage)
 
@@ -196,6 +174,7 @@ class TrainerGridViewAdapter(
         // as change in recycler view data.
         notifyDataSetChanged()
     }
+
     init {
         this.trainerListModels = trainerList
     }
